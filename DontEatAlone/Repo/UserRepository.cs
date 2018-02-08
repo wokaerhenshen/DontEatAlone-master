6172a5bce102756ebc55421bab5df916e2d82e4b
karl <karlxu0410@gmail.com>
2018-02-08 13:11:04 -0800

    add role/userrole feature

---
+++ DontEatAlone/Repo/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using DontEatAlone.Data;
+using DontEatAlone.Models.AccountViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DontEatAlone.Repo
+{
+    public class UserRepository
+    {
+        ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        // Get all users in the databaFse.
+        public IEnumerable<UserVM> All()
+        {
+            var users = _context.Users.Select(u => new UserVM()
+            {
+                Email = u.Email
+            });
+            return users;
+        }
+
+    }
+}
---
+++ DontEatAlone/Repo/UserRoleRepository.cs
@@ -0,0 +1,63 @@
+﻿using DontEatAlone.Models;
+using DontEatAlone.Models.AccountViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DontEatAlone.Repo
+{
+    public class UserRoleRepository
+    {
+        IServiceProvider serviceProvider;
+
+        public UserRoleRepository(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        // Assign a role to a user.
+        public async Task<bool> AddUserRole(string email, string roleName)
+        {
+            var UserManager = serviceProvider
+                                .GetRequiredService<UserManager<ApplicationUser>>();
+            var user = await UserManager.FindByEmailAsync(email);
+            if (user != null)
+            {
+                await UserManager.AddToRoleAsync(user, roleName);
+            }
+            return true;
+        }
+
+        // Remove role from a user.
+        public async Task<bool> RemoveUserRole(string email, string roleName)
+        {
+            var UserManager = serviceProvider
+                                .GetRequiredService<UserManager<ApplicationUser>>();
+            var user = await UserManager.FindByEmailAsync(email);
+            if (user != null)
+            {
+                await UserManager.RemoveFromRoleAsync(user, roleName);
+            }
+            return true;
+        }
+
+        // Get all roles of a specific user.
+        public async Task<IEnumerable<RoleVM>> GetUserRoles(string email)
+        {
+            var UserManager = serviceProvider
+                                .GetRequiredService<UserManager<ApplicationUser>>();
+            var user = await UserManager.FindByEmailAsync(email);
+            var roles = await UserManager.GetRolesAsync(user);
+            List<RoleVM> roleVMObjects = new List<RoleVM>();
+            foreach (var item in roles)
+            {
+                roleVMObjects.Add(new RoleVM() { Id = item, RoleName = item });
+            }
+            return roleVMObjects;
+        }
+
+    }
+}
