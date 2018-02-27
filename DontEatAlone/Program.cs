1ddd4eb8ccacb60638adfea0070a23d794cee1f7
wokaerhenshen <karlxu0410@gmail.com>
2018-02-27 11:28:04 -0800

    add initialize data feature, also include a method that can clean all the initialized data if necessary

--- DontEatAlone/Program.cs
+++ DontEatAlone/Program.cs
@@ -30,6 +30,8 @@ namespace DontEatAlone
                     //initializer.InitializeData();
                     RoleRepo roleIni = new RoleRepo(context);
                     roleIni.CreateInitialRoles();
+                    Initialize DataIni = new Initialize(context);
+                    DataIni.InitializeData();
 
                 }
                 catch (Exception ex)
