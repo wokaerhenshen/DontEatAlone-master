5dd7e895a7e9c67bd0c388a214cdb6cdaa9ef839
Tor Rosa <vrosa2@my.bcit.ca>
2018-02-26 23:44:40 -0800

    Revert "Added login more login authentication, updated ERD"
    This reverts commit 4f83fad5b7f8f13d27cbc5e08ce8a53e0bec16e2.

--- DontEatAlone/Startup.cs
+++ DontEatAlone/Startup.cs
@@ -75,23 +75,6 @@ namespace DontEatAlone
                 options.User.RequireUniqueEmail = true;
             });
 
-            services.Configure<IdentityOptions>(options => {
-                //// Password settings if you want to ensure password strength.
-                //options.Password.RequireDigit           = true;
-                //options.Password.RequiredLength         = 8;
-                //options.Password.RequireNonAlphanumeric = false;
-                //options.Password.RequireUppercase       = true;
-                //options.Password.RequireLowercase       = false;
-                //options.Password.RequiredUniqueChars    = 6;
-
-                // Lockout settings (Freeze 1 minute only to make testing easier)
-                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1);
-                options.Lockout.MaxFailedAccessAttempts = 3; // Lock after 3 consec failed logins
-                options.Lockout.AllowedForNewUsers = true;
-
-                // User settings
-                options.User.RequireUniqueEmail = true;
-            });
 
             services.AddMvc();
             services.Configure<AuthMessageSenderOptions>(Configuration);
